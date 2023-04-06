af018c7630287000bf4331f898940abad73c6e72
Rudeus2909 <124248774+Rudeus2909@users.noreply.github.com>
2023-04-06 11:21:15 +0700

    Add files via upload

---
+++ apps/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+    <p>Bản quyền thuộc Pet Shop</p>
+    <p>Liên hệ: 0886026861</p>
+</div>
---
+++ apps/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Cute Pet</title>
+</head>
+<body>
+    <header>
+        <div>
+            <!-- <div>
+                <marquee behavior="alternative" direction="left">
+                    <strong>
+                        MIỄN PHÍ GIAO HÀNG VỚI HÓA ĐƠN TỪ 3.000.000 VNĐ <span>LIÊN HỆ ĐẶT HÀNG: 0886026861</span>
+                    </strong>
+                </marquee>
+            </div> -->
+            <div style="margin-left:50%">
+                <span style="padding: 10px;">
+                    <i class="fa-solid fa-house"></i>
+                    <a href="index.php?act=home">Trang chủ</a>
+                </span>
+                <span style="padding: 10px;">
+                    <i style="margin-left: 10px;" class="fa-solid fa-cart-shopping"></i>
+                    <a href="index.php?act=fetch">Sản phẩm</a>
+                <span style="padding: 10px;">
+                    <i class="fa-solid fa-user" style="margin-left: 10px;" onclick="click()"></i>
+                    <a href="index.php?act=register" class="a">Đăng ký</a>
+               </span>
+                <span  style="padding: 10px;">
+                    <i style="margin-left: 10px;" class="fa-solid fa-user"></i>
+                    <a href="index.php?act=login" class="a">Đăng nhập</a>
+                </span>
+            </div>
+        </div>
+</body>
+</html>
